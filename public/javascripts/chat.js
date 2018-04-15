f0119d49ea2a0f7df2a033c25617ad2f94d6900b
SUSHMITHAR88 <sushmithar88@gmail.com>
2018-04-15 12:19:53 +0000

    final code2

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,23 +7,23 @@ app.config(function ($mdThemingProvider) {
 });
 
 app.controller('chatController', function ($scope, $sce) {
-    $scope.messages = [];
-    $scope.trust = $sce.trustAsHtml;
+            $scope.messages = [];
+            $scope.trust = $sce.trustAsHtml;
 
-    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+            var exampleSocket = new WebSocket('wss://swiftcode-sushmithar88.herokuapp.com/chatSocket);
 
-    exampleSocket.onmessage = function (event) {
-        var jsonData = JSON.parse(event.data);
-        jsonData.time = new Date()
-            .toLocaleTimeString();
-        $scope.messages.push(jsonData);
-        $scope.$apply();
-        console.log(jsonData);
+                exampleSocket.onmessage = function (event) {
+                    var jsonData = JSON.parse(event.data);
+                    jsonData.time = new Date()
+                        .toLocaleTimeString();
+                    $scope.messages.push(jsonData);
+                    $scope.$apply();
+                    console.log(jsonData);
 
-    };
+                };
 
-    $scope.sendMessage = function () {
-        exampleSocket.send($scope.userMessage);
-        $scope.userMessage = '';
-    };
-});
+                $scope.sendMessage = function () {
+                    exampleSocket.send($scope.userMessage);
+                    $scope.userMessage = '';
+                };
+            });
